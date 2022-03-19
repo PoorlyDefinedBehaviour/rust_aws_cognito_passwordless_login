a133971e36627ab5bff29b56cefe4b070c0dcb96
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2022-03-19 18:29:35 -0300

    ci(terraform): fix typo

--- terraform/lambda_create_auth_challenge.tf
+++ terraform/lambda_create_auth_challenge.tf
@@ -43,7 +43,7 @@ resource "aws_iam_role" "cognito_create_auth_challenge_role" {
       "Resource": "${aws_s3_bucket.cognito_passwordless_signin_lambda_deploys.arn}/${var.cognito_verify_auth_challenge_function_name}",
       "Condition": {
         "StringEquals": {
-          "s3:ResourceAccount": ${var.account_id}
+          "s3:ResourceAccount": "${var.account_id}|"
         }
       }
     },
@@ -52,7 +52,7 @@ resource "aws_iam_role" "cognito_create_auth_challenge_role" {
       "Action": [
         "ses:SendEmail"
       ],
-      "Resource": ${aws_ses_email_identity.ses_email_identity.arn}
+      "Resource": !${aws_ses_email_identity.ses_email_identity.arn}!
     },
     {
       "Effect": "Allow",
@@ -63,7 +63,7 @@ resource "aws_iam_role" "cognito_create_auth_challenge_role" {
       "Resource": "arn:aws:lambda:${var.region}:${var.account_id}:function:${var.cognito_verify_auth_challenge_function_name}",
       "Condition": {
         "ArnLike": {
-          "AWS:SourceArn": ${aws_cognito_user_pool.cognito_user_pool.arn}
+          "AWS:SourceArn": "${aws_cognito_user_pool.cognito_user_pool.arn}"
         }
       }
     }
