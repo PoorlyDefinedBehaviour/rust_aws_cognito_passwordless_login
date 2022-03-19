2cfe18377a65e9b91684692cf2118735ccf0d692
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2022-03-19 16:07:45 -0300

    fix(terraform): forgot function segment in lambda_config

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -1,9 +1,9 @@
 resource "aws_cognito_user_pool" "cognito_user_pool" {
   name = "cognito-passwordless-user-pool"
   lambda_config {
-    pre_sign_up                    = "arn:aws:lambda:${var.region}:${var.account_id}:${var.cognito_pre_signup_function_name}"
-    define_auth_challenge          = "arn:aws:lambda:${var.region}:${var.account_id}:${var.cognito_define_auth_challenge_function_name}"
-    create_auth_challenge          = "arn:aws:lambda:${var.region}:${var.account_id}:${var.cognito_create_auth_challenge_function_name}"
-    verify_auth_challenge_response = "arn:aws:lambda:${var.region}:${var.account_id}:${var.cognito_verify_auth_challenge_function_name}"
+    pre_sign_up                    = "arn:aws:lambda:${var.region}:${var.account_id}:function:${var.cognito_pre_signup_function_name}"
+    define_auth_challenge          = "arn:aws:lambda:${var.region}:${var.account_id}:function:${var.cognito_define_auth_challenge_function_name}"
+    create_auth_challenge          = "arn:aws:lambda:${var.region}:${var.account_id}:function:${var.cognito_create_auth_challenge_function_name}"
+    verify_auth_challenge_response = "arn:aws:lambda:${var.region}:${var.account_id}:function:${var.cognito_verify_auth_challenge_function_name}"
   }
 }
