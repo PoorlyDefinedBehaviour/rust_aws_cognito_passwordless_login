201484145cf525028a8fa957c28cdcf7780ccb20
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2022-03-19 16:15:49 -0300

    fix(terraform): make s3 acl private

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,8 +1,12 @@
-resource "aws_s3_bucket" "cognito_passwordless_signin_lambda_deploys" {
-  bucket = "cognito-passwordless-signin-lambda-deploys"
+variable "cognito_passwordless_signin_lambda_deploys_bucket_name" {
+  default = "cognito-passwordless-signin-lambda-deploys"
+}
 
+resource "aws_s3_bucket" "cognito_passwordless_signin_lambda_deploys" {
+  bucket = var.cognito_passwordless_signin_lambda_deploys_bucket_name
+  acl    = "private"
   tags = {
-    Name = "cognito-passwordless-signin-lambda-deploys"
+    Name = var.cognito_passwordless_signin_lambda_deploys_bucket_name
   }
 }
 
