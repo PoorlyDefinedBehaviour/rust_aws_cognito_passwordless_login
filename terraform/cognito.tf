c2a77a9109da6112b29010b13754418d930e34ca
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2022-03-22 23:31:23 -0300

    fix: replace cognito_user_pool with the one created by terraformer

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -1,46 +1,4 @@
 resource "aws_cognito_user_pool" "cognito_user_pool" {
-  name = "cognito-passwordless-user-pool"
-  username_configuration {
-    case_sensitive = false
-  }
-
-  username_attributes = ["email", "phone_number"]
-
-  schema {
-    name                     = "email"
-    attribute_data_type      = "String"
-    developer_only_attribute = false
-    mutable                  = false
-    required                 = true
-    string_attribute_constraints {
-      min_length = 1
-      max_length = 256
-    }
-  }
-
-
-  schema {
-    name                     = "phone_number"
-    attribute_data_type      = "String"
-    developer_only_attribute = false
-    mutable                  = false
-    required                 = false
-    string_attribute_constraints {
-      min_length = 1
-      max_length = 256
-    }
-  }
-
-  lambda_config {
-    pre_sign_up                    = "arn:aws:lambda:${var.region}:${var.account_id}:function:${var.cognito_pre_signup_function_name}"
-    define_auth_challenge          = "arn:aws:lambda:${var.region}:${var.account_id}:function:${var.cognito_define_auth_challenge_function_name}"
-    create_auth_challenge          = "arn:aws:lambda:${var.region}:${var.account_id}:function:${var.cognito_create_auth_challenge_function_name}"
-    verify_auth_challenge_response = "arn:aws:lambda:${var.region}:${var.account_id}:function:${var.cognito_verify_auth_challenge_function_name}"
-  }
-}
-
-
-resource "aws_cognito_user_pool" "tfer--TestPool123" {
   account_recovery_setting {
     recovery_mechanism {
       name     = "verified_email"
